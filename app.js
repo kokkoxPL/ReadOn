e7c0a264379f41e701ca5641fc96a303033a8b2b
kokkoxPL <adriangamracy@gmail.com>
2022-04-21 02:04:46 +0200

    test

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ const app = express();
 
 const hash = (password) => crypto.createHash("sha256").update(password).digest("base64");
 
-// mongoose
-//     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => )
-//     .catch((err) => console.log(err));
-app.listen(process.env.PORT)
+mongoose
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(process.env.PORT))
+    .catch((err) => console.log(err));
+
 const msgs = {
     1: "Successfully printed a book",
     2: "Successfully reprinted a book",
     3: "Book cover couldn't be uploaded",
 };
 
-//app.use(compression());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     req.isVerified = req.cookies.access_token == hash(process.env.ADMIN_PASSWORD);
     req.msg = req.query.msg && msgs[req.query.msg] ? msgs[req.query.msg] : "";
     req.log = (type, data) => {
@@ -37,13 +37,13 @@ app.use(cookieParser());
         log.save();
     };
     next();
-});*/
+});
 
 app.set("view engine", "ejs");
 
-//app.use(bookRoutes);
+app.use(bookRoutes);
 
-//app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use((req, res) => {
     res.render("404");
