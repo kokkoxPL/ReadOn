32dcf4c3e6330862e5bf8c13e07836ae89a8019b
kokkoxPL <adriangamracy@gmail.com>
2022-05-06 22:54:36 +0200

    tweaked adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,6 +5,8 @@ const fetch = require("node-fetch");
 const formData = require("form-data");
 const crypto = require("crypto");
 
+const hash = (password) => crypto.createHash("sha256").update(password).digest("base64");
+
 async function uploadImageToImgur(buffer) {
     const data = new formData();
     data.append("image", buffer);
@@ -30,37 +32,31 @@ async function deleteImageFromImgur(deleteHash) {
     return json;
 }
 
-const hash = (password) => crypto.createHash("sha256").update(password).digest("base64");
-
 const get_admin = (req, res) => {
     if (req.isVerified) {
         Book.find()
             .sort({ title: 1 })
             .then((result) => res.render("admin", { books: result }))
             .catch((err) => res.render("404", { err }));
-    } else {
+    } else
         res.redirect("/admin/login");
-    }
 };
 
 const get_admin_login = (req, res) => {
-    if (req.isVerified) {
+    if (req.isVerified)
         res.redirect("/admin");
-    } else {
+    else
         res.render("login");
-    }
 };
 
 const post_admin_login = (req, res) => {
     const { password } = req.body;
-    if (!password) {
+    if (!password)
         return res.sendStatus(400);
-    }
-    if (password !== process.env.ADMIN_PASSWORD) {
+    if (password !== process.env.ADMIN_PASSWORD)
         return res.sendStatus(401);
-    }
-    const passHash = hash(password);
-    res.cookie("access_token", passHash);
+
+    res.cookie("access_token", hash(password));
     res.redirect("/admin");
 };
 
@@ -70,34 +66,33 @@ const get_admin_new_book = (req, res) => {
             .sort({ name: 1 })
             .then((result) => res.render("adminNew", { msg: req.msg, tags: result }))
             .catch((err) => res.render("404", { err }));
-    } else {
+    } else
         res.redirect("/admin/login");
-    }
 };
 
 const post_admin_new_book = async (req, res) => {
     if (req.isVerified) {
         const data = req.body;
+
         if (req.file) {
             const buffer = req.file.buffer;
             const imgurResponse = await uploadImageToImgur(buffer);
+
             if (imgurResponse.success === true) {
                 data.imgLink = imgurResponse.data.link;
                 data.imgDeleteHash = imgurResponse.data.deletehash;
-            } else {
+            } else
                 return res.redirect("/admin/new?msg=3");
-            }
-        } else {
+        } else
             data.imgLink = "https://upload.wikimedia.org/wikipedia/commons/c/c8/Cards-Blank.svg";
-        }
-        console.log(data);
+
         const book = new Book(data);
+
         book.save()
             .then((result) => res.redirect("/admin/new?msg=1"))
             .catch((err) => res.render("404", { err }));
-    } else {
+    } else
         res.redirect("/login");
-    }
 };
 
 const get_admin_edit_books = (req, res) => {
@@ -105,9 +100,8 @@ const get_admin_edit_books = (req, res) => {
         Book.find()
             .then((result) => res.render("adminEdit", { books: result, msg: req.msg }))
             .catch((err) => res.render("404", { err }));
-    } else {
+    } else
         res.redirect("/admin/login");
-    }
 };
 
 const get_admin_edit_id_book = (req, res) => {
@@ -126,25 +120,26 @@ const get_admin_edit_id_book = (req, res) => {
 const post_admin_edit_id_book = async (req, res) => {
     if (req.isVerified) {
         const data = req.body;
+
         if (req.file) {
             const buffer = req.file.buffer;
             const imgurResponse = await uploadImageToImgur(buffer);
+
             if (imgurResponse.success === true) {
                 data.imgLink = imgurResponse.data.link;
                 data.imgDeleteHash = imgurResponse.data.deletehash;
-            } else {
+            } else
                 return res.redirect("/admin/edit?msg=3");
-            }
         }
         Book.findOneAndUpdate({ _id: req.params.id }, data)
             .then((previousResult) => {
                 res.redirect(`/admin/edit/${req.params.id}?msg=2`);
                 req.log("book_edited", { from: previousResult, to: data });
+
                 if (previousResult.imgDeleteHash && req.file) {
                     deleteImageFromImgur(previousResult.imgDeleteHash).then((result) => {
-                        if (result.success === false) {
+                        if (result.success === false)
                             console.log("Couldn't delete image after edit");
-                        }
                     });
                 }
             })
@@ -153,20 +148,17 @@ const post_admin_edit_id_book = async (req, res) => {
 };
 
 const get_admin_delete_book = (req, res) => {
-    if (req.isVerified) {
+    if (req.isVerified)
         Book.find().then((result) => res.render("adminDelete", { books: result }));
-    }
 };
 
 const post_admin_delete_book = (req, res) => {
     if (req.isVerified) {
         Book.findById(req.body.id).then((result) => {
-            console.log(result);
             if (result.imgDeleteHash) {
                 deleteImageFromImgur(result.imgDeleteHash).then((result) => {
-                    if (result.success === false) {
+                    if (result.success === false)
                         console.log("Couldn't delete image");
-                    }
                 });
             }
             Book.findByIdAndDelete(req.body.id).then(() => res.redirect("/admin/delete?msg=1"));
@@ -175,20 +167,15 @@ const post_admin_delete_book = (req, res) => {
 };
 
 const get_admin_tags = (req, res) => {
-    if (req.isVerified) {
-        Tag.find().then((results) => {
-            res.render("tagNew", { tags: results });
-        });
-    }
+    if (req.isVerified)
+        Tag.find().then((results) => res.render("tagNew", { tags: results }));
 };
 
 const post_admin_tags = (req, res) => {
     if (req.isVerified) {
         const { tagName } = req.body;
         const tag = new Tag({ name: tagName });
-        tag.save().then(() => {
-            res.redirect("/admin/tags");
-        });
+        tag.save().then(() => res.redirect("/admin/tags"));
     }
 };
 
@@ -200,11 +187,8 @@ const post_admin_tags_delete = (req, res) => {
 };
 
 const get_admin_logs = (req, res) => {
-    if (req.isVerified) {
-        Log.find().then((result) => {
-            res.render("logs", { logs: result });
-        });
-    }
+    if (req.isVerified)
+        Log.find().then((result) => res.render("logs", { logs: result }));
 };
 
 const get_admin_logout = (req, res) => {
